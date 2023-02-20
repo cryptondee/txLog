aa38d7e2618a7077891232292b8c804433e4aae7
NDW <cryptondee@gmail.com>
2023-02-20 22:12:56 +0100

    final

--- pages/test.js
+++ pages/test.js
@@ -3,39 +3,70 @@ import React, { useState, useEffect } from "react";
 
 function Test() {
   const [transactions, setTransactions] = useState([]);
-  useEffect(() => {
-    const getTransactions = async () => {
-      const config = {
-        apiKey: process.env.alchemyApi,
-        network: Network.ARB_MAINNET,
-      };
-      const alchemy = new Alchemy(config);
+  const [toAddress, setToAddress] = useState();
+  const [wsEnabled, setWsEnabled] = useState(false);
+  const [text, setText] = useState("not watching");
 
-      alchemy.ws.on(
-        {
-          method: AlchemySubscription.MINED_TRANSACTIONS,
-          addresses: [
-            {
-              to: "0x87898B0d8aB25F5034e045d056027647C5494584",
-            },
-          ],
-          includeRemoved: true,
-          hashesOnly: false,
-        },
-        (tx) => {
-          console.log(tx);
-          setTransactions((prevTx) => [...prevTx, tx]);
-        }
-      );
+  const getTransactions = async () => {
+    const config = {
+      apiKey: process.env.alchemyApi,
+      network: Network.ARB_MAINNET,
     };
+    const alchemy = new Alchemy(config);
+
+    alchemy.ws.on(
+      {
+        method: AlchemySubscription.MINED_TRANSACTIONS,
+        addresses: [
+          {
+            to: toAddress,
+          },
+        ],
+        includeRemoved: true,
+        hashesOnly: false,
+      },
+      (tx) => {
+        console.log(tx);
+        setTransactions((prevTx) => [...prevTx, tx]);
+      }
+    );
+  };
+
+  useEffect(() => {
+    if (!wsEnabled) {
+      return;
+    }
 
     getTransactions();
-  }, []);
+    setText("Watching");
+  }, [toAddress, wsEnabled]);
+  const handleInputChange = (event) => {
+    setToAddress(event.target.value);
+    setWsEnabled(false);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setWsEnabled(true);
+  };
+
   return (
     <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="to-address-input">To Address:</label>
+          <input
+            type="text"
+            id="to-address-input"
+            value={toAddress}
+            onChange={handleInputChange}
+          />
+        </div>
+      </form>
+
       {transactions.length > 0 ? (
         transactions.map((tx) => (
-          <div key={tx.hash}>
+          <div key={tx.transaction.hash}>
             <p>Hash: {tx.transaction.hash}</p>
             <p>From: {tx.transaction.from}</p>
             <p>To: {tx.transaction.to}</p>
@@ -43,7 +74,7 @@ function Test() {
           </div>
         ))
       ) : (
-        <p>No transactions yet.</p>
+        <p>{text}</p>
       )}
     </div>
   );
